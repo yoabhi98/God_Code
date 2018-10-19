d28ff0de962b95843a413ba8f1b8f9dbec34fb27
yoabhi98 <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 14:03:57 +0530

    Add files via upload

---
+++ Leap_year.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Leap_Year
+{
+
+     
+public static void main(String []args)
+{
+               
+Scanner sc=new Scanner(System.in);
+      
+int year = sc.nextInt(); 
+
+if(year%4==0 && year%100!=0 || year%400==0)
+System.out.println("yes"); 
+else
+System.out.println("no");     
+}
+}

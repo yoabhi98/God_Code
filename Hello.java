7526f71390ca9b37724e35f6ac06e01f4519088f
yoabhi98 <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 14:07:10 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Hello
+{
+
+     
+public static void main(String []args)
+{
+               
+Scanner sc=new Scanner(System.in);
+       
+int number = sc.nextInt(); 
+
+for(int i=1;i<=number;i++)
+System.out.println("Hello");      
+}
+}

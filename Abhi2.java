5336b76aa59e77b62d57c36a64353e318c55de27
yoabhi98 <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 13:03:50 +0530

    Add files via upload

---
+++ Abhi2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Abhi2
+{
+
+     
+public static void main(String []args)
+{
+        
+System.out.println("Enter a Character");
+        
+Scanner sc=new Scanner(System.in);
+       
+char n = sc.next().charAt(0); 
+
+if((n>='a'&&n<='z')||(n>='A'&&n<='Z'))
+System.out.println("Alaphabet"); 
+else
+System.out.println("No");
+           
+}
+}

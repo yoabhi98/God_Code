dfadd5ee2ef6a2b919f00549e4f9b8d6c6dbf715
yoabhi98 <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 11:46:27 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class HelloWorld
+{
+
+     
+public static void main(String []args)
+{
+        
+System.out.println("Enter a Number");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+if(n==0)
+         
+System.out.println("Number is zero");
+        else if(n>0)
+         System.out.println("Number is Positive");
+         
+else
+          
+System.out.println("Number is Negative");
+         
+     
+}
+}

861d63a047de24f97062169982cfc4a9c76c0d32
yoabhi98 <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 13:51:11 +0530

    Add files via upload

---
+++ Abhi5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Abhi5
+{
+
+     
+public static void main(String []args)
+{
+               
+Scanner sc=new Scanner(System.in);
+       
+int n = sc.nextInt(); 
+
+for(int i=1;i<=n;i++)
+System.out.println("Hello");      
+}
+}

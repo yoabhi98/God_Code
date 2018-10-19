5082b5860b43fbd0b900282552693db97d022ee0
yoabhi98 <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 14:35:22 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Sum
+{
+
+     
+public static void main(String []args)
+{
+               
+Scanner sc=new Scanner(System.in);
+
+int sum=0;
+int n = sc.nextInt();
+int arr[]=new int[n];       
+int a=sc.nextInt(); 
+
+for(int i=0;i<n;i++)
+arr[i]=sc.nextInt();
+for(int i=0;i<a;i++)
+sum+=arr[i];
+System.out.println(sum);      
+}
+}

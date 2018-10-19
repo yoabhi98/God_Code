043222f67970d9bb14b6cb3646187a9ed8ca0c1b
yoabhi98 <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 13:46:38 +0530

    Add files via upload

---
+++ Abhi4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Abhi4
+{
+
+     
+public static void main(String []args)
+{
+        
+System.out.println("Enter Year");
+        
+Scanner sc=new Scanner(System.in);
+       
+int yy = sc.nextInt(); 
+
+if(yy%4==0 && yy%100!=0 || yy%400==0)
+System.out.println("yes"); 
+else
+System.out.println("no");     
+}
+}

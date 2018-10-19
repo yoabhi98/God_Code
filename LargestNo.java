2074b4c5b9b905d5fff6f9b73f1710d5391c8d0a
yoabhi98 <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 13:59:18 +0530

    Add files via upload

---
+++ LargestNo.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class LargestNo
+{
+
+     
+public static void main(String []args)
+{
+                
+Scanner sc=new Scanner(System.in);
+      
+int a1 = sc.nextInt(); 
+
+int b1 = sc.nextInt(); 
+
+int c1 = sc.nextInt(); 
+
+if(a1>b1 && a>c1)
+System.out.println(a1); 
+else if(b1>a && b1>c1)
+System.out.println(b1);
+else
+System.out.println(c1);
+           
+}
+}

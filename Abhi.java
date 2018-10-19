975125c1fa188a149b0a047755648543c2ee8ab7
yoabhi98 <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 11:58:38 +0530

    Add files via upload

---
+++ Abhi.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Abhi
+{
+
+     
+public static void main(String []args)
+{
+ 
+try{       
+System.out.println("Enter a Number");
+        
+Scanner sc=new Scanner(System.in);
+       
+ int n=sc.nextInt();
+       
+if(n%2==0)
+         
+System.out.println("Number is Even");
+        
+else        
+System.out.println("Number is Odd");
+ 
+}
+catch(Exception e)
+{
+System.out.println("Invalid Number");
+         
+     
+}        
+}
+}

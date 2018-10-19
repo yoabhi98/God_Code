d96817185fe86f016ce7f5870fa2b383fb144b53
yoabhi98 <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 14:42:25 +0530

    Add files via upload

---
+++ Count.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Count
+{
+
+     
+public static void main(String []args)
+{
+               
+Scanner sc=new Scanner(System.in);
+
+int count=0;
+int n = sc.nextInt();
+while(n!=0)
+{
+n/=10;
+count++;
+} 
+System.out.println(count);     
+}
+}

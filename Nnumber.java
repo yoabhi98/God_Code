25366498dacd5b6b4f39e6872d99fdfc858c9f98
yoabhi98 <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 14:14:42 +0530

    Add files via upload

---
+++ Nnumber.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Nnumber
+{
+
+     
+public static void main(String []args)
+{
+               
+Scanner sc=new Scanner(System.in);
+       int a=0;
+int n = sc.nextInt(); 
+
+for(int i=1;i<=n;i++)
+a+=i;
+System.out.println(a);      
+}
+}

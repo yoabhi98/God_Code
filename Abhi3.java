6bc5ef3116019e0fe797946b6d31434ae4344174
yoabhi98 <42784341+yoabhi98@users.noreply.github.com>
2018-10-19 13:36:58 +0530

    Add files via upload

---
+++ Abhi3.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Abhi3
+{
+
+     
+public static void main(String []args)
+{
+        
+System.out.println("Enter Three Number");
+        
+Scanner sc=new Scanner(System.in);
+       
+int a = sc.nextInt(); 
+
+int b = sc.nextInt(); 
+
+int c = sc.nextInt(); 
+
+if(a>b && a>c)
+System.out.println(a); 
+else if(b>a && b>c)
+System.out.println(b);
+else
+System.out.println(c);
+           
+}
+}
